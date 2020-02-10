16a91e7e97d27d6c791ecf637c115e80b88c1386
Jason Lin <amigo_brian@hotmail.com>
2020-02-11 01:24:56 +0800

    first verison

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+import styled from 'styled-components'
+import ReactDOM from 'react-dom'
+import styles from './App.module.css'
 
-function App() {
+const PICTURE_WIDTH = 800
+
+const PicContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 150px;
+  padding-bottom: 50px;
+`
+const CarouselBackground = styled.div`
+  background-image: url(/overlay.png), url(/bg.jpg);
+  background-repeat: repeat, repeat-x;
+  background-size: 128px 128px, cover;
+`
+export const App = () => {
+  const WrapRef = useRef(null)
+  let appDom
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    appDom = ReactDOM.findDOMNode(WrapRef.current)
+    if (!appDom) {
+      return
+    }
+    const carousel = appDom.querySelector('div.carousel')
+    carousel.classList.add(styles.boxShadow)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <CarouselBackground>
+      <PicContainer ref={WrapRef}>
+        <Carousel
+          showArrows={true}
+          width={PICTURE_WIDTH}
+          showStatus={false}
+          infiniteLoop={true}
+          onChange={(i) => {
+            setIndex(i)
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <img src="/content1.jpg" alt="content1" />
+            <p className="legend">Our Cat</p>
+          </div>
+          <div>
+            <img src="/content2.jpg" alt="content2" />
+            <p className="legend">The Beautiful School</p>
+          </div>
+        </Carousel>
+      </PicContainer>
+    </CarouselBackground>
+  )
 }
-
-export default App;
